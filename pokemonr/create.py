a256d807f44fecab0b1805107854be3e582ec55e
Ryan Nevils <ryan.nevils21@gmail.com>
2023-07-27 22:19:14 -0400

    incremental updates

--- pokemonr/create.py
+++ pokemonr/create.py
@@ -2,85 +2,117 @@ import functools
 import re
 import json
 
-from flask import (
-    Blueprint, flash, g, render_template, request, session
-)
+from flask import Blueprint, flash, g, render_template, request, session
 
 
-bp = Blueprint('create', __name__)
+bp = Blueprint("create", __name__)
 
 
-@bp.route('/')
+@bp.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
 
 
-@bp.route('/create', methods=['POST'])
+@bp.route("/results", methods=["POST"])
 def create():
-    body = request.form['body']
-    names,data = create_data(body)
+    body = request.form["body"]
+    names, data = create_data(body)
 
-    return render_template('results.html', pokemons=names, weaknesses=data)
+    return render_template(
+        "results.html", pokemons=names, weaknesses=data, get_class=get_class
+    )
 
+
+def get_class(matchup):
+    match matchup:
+        case 0:
+            return "no-effect"
+        case 0.5:
+            return "not-very-effective"
+        case 0.25:
+            return "very-not-very-effective"
+        case 2:
+            return "super-effective"
+        case 4:
+            return "very-super-effective"
+        case _:
+            return "normal-effective"
+
+    return "matchup-20"
 
 
 def create_data(body):
-
-    final_data = {}
-
-    # parse input and get names
     names = []
-    mons = body.split("\r\n\r\n") #TODO is this gonna work on all platforms
+    mons = body.split("\r\n\r\n")  # TODO is this gonna work on all platforms
 
     for mon in mons:
         mon_no_item_split = mon.split("\r\n")[0].split("@")[0].split(" ")
         for chunk in reversed(mon_no_item_split):
-            if chunk not in ['(F)', '', '(M)']:
+            if chunk not in ["(F)", "", "(M)"]:
                 names.append(chunk.strip("()"))
                 break
 
-
-    # read my file of weaknesses and get appropriate entries
-    with open('data.json', encoding="utf-8") as f:
+    with open("data.json", encoding="utf-8") as f:
         data = json.load(f)
 
     result = []
     for name in names:
         result.append(data[name])
 
+    pokemon_types = [
+        "Normal",
+        "Fire",
+        "Water",
+        "Electric",
+        "Grass",
+        "Ice",
+        "Fighting",
+        "Poison",
+        "Ground",
+        "Flying",
+        "Psychic",
+        "Bug",
+        "Rock",
+        "Ghost",
+        "Dragon",
+        "Dark",
+        "Steel",
+    ]
 
-    pokemon_types = ["Normal", "Fire", "Water", "Electric", "Grass", "Ice", "Fighting", "Poison", "Ground", "Flying", "Psychic", "Bug", "Rock", "Ghost", "Dragon", "Dark", "Steel"]
-
+    final_data = {}
     for idx, pokemon_type in enumerate(pokemon_types):
-        final_data[pokemon_type] = []
+        matchups = []
+        weakness_data = {}
         for weaknesses in result:
             if weaknesses[idx] == 1 or weaknesses[idx] == 1.0:
-                final_data[pokemon_type].append("")
+                matchups.append("")
             else:
-                final_data[pokemon_type].append(weaknesses[idx])
+                matchups.append(weaknesses[idx])
+        weakness_data["matchups"] = matchups
+        weakness_data["Total_Weak"] = ""
+        weakness_data["Total_Resist"] = ""
+        final_data[pokemon_type] = weakness_data
 
-
-    # calculate total weak and total resist
     for type in final_data:
-
         num_weak = 0
         num_resist = 0
         # append total weak
-        for num in final_data[type]:
-            if num == 2 or num == 4 or num == 2.0:
+        for num in final_data[type]["matchups"]:
+            if num in [2, 2.0, 4]:
                 num_weak += 1
-            elif num == 0.5 or num == 0.25:
+            elif num in [0.5, 0.25, 0]:
                 num_resist += 1
 
-
         if num_weak == 0:
-            final_data[type].append("")
+            final_data[type]["Total_Weak"] = ""
         else:
-            final_data[type].append(num_weak)
-    
-        if num_resist == 0:
-            final_data[type].append("")
-        else:
-            final_data[type].append(num_resist)
+            final_data[type]["Total_Weak"] = num_weak
 
-    return names,final_data
+        if num_resist == 0:
+            final_data[type]["Total_Resist"] = ""
+        else:
+            final_data[type]["Total_Resist"] = num_resist
+
+    print(final_data)
+
+    return names, final_data
